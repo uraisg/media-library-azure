f66680931d0baf14a4524b80a5c6292c3dd594bf
Rafferty Uy <rafferty.uy@gmail.com>
2020-07-21 14:25:47 +0800

    Initial internet project template

---
+++ internet-webapp/MediaLibrary.Internet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MediaLibrary.Internet.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
