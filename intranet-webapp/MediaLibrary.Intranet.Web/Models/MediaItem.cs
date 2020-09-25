4703ef55ba5ee7047336d2518a61dd9331767f8a
Rafferty Uy <1037626+raffertyuy@users.noreply.github.com>
2020-09-25 20:16:33 +0800

    Used Azure Search SDK for searching (#14)

---
+++ intranet-webapp/MediaLibrary.Intranet.Web/Models/MediaItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Search;
+using Microsoft.Spatial;
+
+namespace MediaLibrary.Intranet.Web.Models
+{
+    public class MediaItem
+    {
+        [IsSearchable, IsSortable]
+        public string Name { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public DateTime DateTaken { get; set; }
+        
+        public GeographyPoint Location { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string[] Tag { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public DateTime UploadDate { get; set; }
+
+        [IsSearchable, IsFilterable]
+        public string FileUrl { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string Project { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string Event { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+        public string LocationName { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string Copyright { get; set; }
+    }
+}
