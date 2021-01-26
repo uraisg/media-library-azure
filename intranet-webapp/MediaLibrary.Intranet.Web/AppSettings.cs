e512eafd33711d9b6c0cfba0a745fa74152abb2e
Teo Kah Hou <khteo@users.noreply.github.com>
2021-01-26 17:16:10 +0800

    intranet: clean up app settings

--- intranet-webapp/MediaLibrary.Intranet.Web/AppSettings.cs
+++ intranet-webapp/MediaLibrary.Intranet.Web/AppSettings.cs
@@ -8,8 +8,6 @@ namespace MediaLibrary.Intranet.Web
     public class AppSettings
     {
         public string MediaStorageConnectionString { get; set; }
-        public string MediaStorageAccountName { get; set; }
-        public string MediaStorageAccountKey { get; set; }
         public string MediaStorageImageContainer { get; set; }
         public string MediaStorageIndexContainer { get; set; }
         public string InternetTableAPI { get; set; }
