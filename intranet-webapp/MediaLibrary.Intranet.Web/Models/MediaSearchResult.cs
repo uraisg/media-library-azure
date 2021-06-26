e3a5ea3bf60baab2a6f2041842fca8808363dcde
Teo Kah Hou <khteo@users.noreply.github.com>
2021-06-27 00:53:29 +0800

    feat: intranet: Create MediaSearchService to interface with Cognitive Search

---
+++ intranet-webapp/MediaLibrary.Intranet.Web/Models/MediaSearchResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Search.Models;
+
+namespace MediaLibrary.Intranet.Web.Models
+{
+    public class MediaSearchResult
+    {
+        public MediaSearchResult(DocumentSearchResult<MediaItem> result = null)
+        {
+            if (result != null)
+            {
+                Items = result.Results.Select(x => x.Document).ToList();
+                Total = result.Count;
+            }
+            else
+            {
+                Items = new List<MediaItem>();
+                Total = 0;
+            }
+        }
+        public IList<MediaItem> Items { get; set; }
+
+        public int Count => Items.Count;
+
+        public long? Total { get; set; }
+    }
+
+    public class MediaSearchOptions
+    {
+        public IList<string> LocationFilter { get; set; }
+
+        public IList<string> TagFilter { get; set; }
+
+        public string SpatialFilter { get; set; }
+
+        // Unix timestamp representation (seconds)
+        public long? MinDateTaken { get; set; }
+
+        // Unix timestamp representation (seconds)
+        public long? MaxDateTaken { get; set; }
+    }
+}
