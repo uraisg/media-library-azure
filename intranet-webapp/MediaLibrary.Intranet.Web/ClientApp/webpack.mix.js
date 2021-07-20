9e26684768350e1190534df3e429c7f2226d2599
Teo Kah Hou <khteo@users.noreply.github.com>
2021-07-20 22:22:04 +0800

    Adjust mix output path
    Remove `dist` folder

--- intranet-webapp/MediaLibrary.Intranet.Web/ClientApp/webpack.mix.js
+++ intranet-webapp/MediaLibrary.Intranet.Web/ClientApp/webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix')
 const path = require('path')
 
-mix.setPublicPath(path.normalize('../wwwroot/js/dist'))
+mix.setPublicPath(path.normalize('../wwwroot/js'))
 mix.webpackConfig({
   externals: {
     jquery: 'jQuery',
@@ -16,4 +16,4 @@ mix.webpackConfig({
 mix.babelConfig({
   plugins: ['transform-class-properties'],
 })
-mix.js('src/main.js', '../wwwroot/js/dist').react()
+mix.js('src/main.js', '../wwwroot/js/main.min.js').react()
