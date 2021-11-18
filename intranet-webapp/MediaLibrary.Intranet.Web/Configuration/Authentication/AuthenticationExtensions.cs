3deb02e9b3cc9f910e9a5c021b3b5045592c69cf
Teo Kah Hou <khteo@users.noreply.github.com>
2021-11-18 15:23:42 +0800

    feat(authn): Integrate intranet app with Microsoft Graph
    * Retrieve basic user profile from Microsoft Graph after sign in and add to identity claims
     * Update navbar to show signed-in user's name, email and profile photo

--- intranet-webapp/MediaLibrary.Intranet.Web/Configuration/Authentication/AuthenticationExtensions.cs
+++ intranet-webapp/MediaLibrary.Intranet.Web/Configuration/Authentication/AuthenticationExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using MediaLibrary.Intranet.Web.Common;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
@@ -6,7 +8,9 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Graph;
 using Microsoft.Identity.Web;
 
 namespace MediaLibrary.Intranet.Web.Configuration
@@ -14,6 +18,10 @@ namespace MediaLibrary.Intranet.Web.Configuration
     public static class AuthenticationExtensions
     {
         private static readonly TimeSpan TicketExpiry = TimeSpan.FromMinutes(30);
+        private static readonly string[] DefaultGraphScopes = {
+            "User.Read",
+            "User.ReadBasic.All"
+        };
 
         public static IServiceCollection AddCustomAuthenticationConfig(this IServiceCollection services, IConfiguration config)
         {
@@ -25,6 +33,7 @@ namespace MediaLibrary.Intranet.Web.Configuration
                     options =>
                     {
                         config.Bind("AzureAd", options);
+                        options.Events.OnTokenValidated = context => GetGraphClaims(context);
                     },
                     // Cookie authentication options
                     options =>
@@ -40,7 +49,17 @@ namespace MediaLibrary.Intranet.Web.Configuration
 
                         // Use in-memory ticket store
                         options.SessionStore = new MemoryCacheTicketStore(TicketExpiry);
-                    });
+                    })
+                // Add ability to call web API (Graph) and get access tokens
+                .EnableTokenAcquisitionToCallDownstreamApi(DefaultGraphScopes)
+                // Add a GraphServiceClient via dependency injection
+                .AddMicrosoftGraph(options =>
+                {
+                    options.Scopes = string.Join(' ', DefaultGraphScopes);
+                })
+                // Use in-memory token cache
+                // See https://github.com/AzureAD/microsoft-identity-web/wiki/token-cache-serialization
+                .AddInMemoryTokenCaches();
 
             return services;
         }
@@ -55,6 +74,65 @@ namespace MediaLibrary.Intranet.Web.Configuration
             );
 
             return builder;
+        }
+
+        private static async Task GetGraphClaims(TokenValidatedContext context)
+        {
+            var tokenAcquisition = context.HttpContext.RequestServices
+                .GetRequiredService<ITokenAcquisition>();
+
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(async (request) =>
+                {
+                    var token = await tokenAcquisition
+                        .GetAccessTokenForUserAsync(DefaultGraphScopes, user: context.Principal);
+                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                })
+            );
+
+            // Get user information from Graph
+            var user = await graphClient.Me.Request()
+                .Select(u => new
+                {
+                    u.DisplayName,
+                    u.Mail,
+                    u.UserPrincipalName
+                })
+                .GetAsync();
+
+            context.Principal.AddUserGraphInfo(user);
+
+            if (context.Principal.IsPersonalAccount())
+            {
+                // Personal accounts do not support getting their photo via Graph
+                // Support is there in the beta API
+                context.Principal.AddUserGraphPhoto(null);
+                return;
+            }
+
+            // Get the user's photo
+            // If the user doesn't have a photo, this throws
+            try
+            {
+                var req = graphClient.Me
+                    .Photos["48x48"]
+                    .Content
+                    .Request();
+
+                var photo = await req.GetAsync();
+
+                context.Principal.AddUserGraphPhoto(photo);
+            }
+            catch (ServiceException ex)
+            {
+                if (!ex.IsMatch("ErrorItemNotFound"))
+                {
+                    var loggerFactory = context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>();
+                    var logger = loggerFactory.CreateLogger<Startup>();
+                    logger.LogError(ex, "Error getting signed-in user profile photo");
+                }
+                context.Principal.AddUserGraphPhoto(null);
+            }
         }
     }
 }
