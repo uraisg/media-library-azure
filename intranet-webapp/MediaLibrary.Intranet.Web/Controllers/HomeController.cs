ce2256060a6c1faa8f3d885f5213f2a20b3eff67
Teo Kah Hou <khteo@users.noreply.github.com>
2022-02-07 11:47:07 +0800

    feat: Redirect intranet home page to new search page

--- intranet-webapp/MediaLibrary.Intranet.Web/Controllers/HomeController.cs
+++ intranet-webapp/MediaLibrary.Intranet.Web/Controllers/HomeController.cs
@@ -18,182 +18,21 @@ namespace MediaLibrary.Intranet.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly AppSettings _appSettings;
-        private readonly IGeoSearchHelper _geoSearchHelper;
 
-        private static SearchServiceClient _searchServiceName = null;
-        private static ISearchIndexClient _searchIndexClient = null;
-
-        public HomeController(IOptions<AppSettings> appSettings, ILogger<HomeController> logger, IGeoSearchHelper geoSearchHelper)
+        public HomeController(ILogger<HomeController> logger)
         {
-            _appSettings = appSettings.Value;
             _logger = logger;
-            _geoSearchHelper = geoSearchHelper;
-
-            InitSearch();
         }
 
-        private void InitSearch()
+        public IActionResult Index()
         {
-            if (_searchServiceName == null)
-                _searchServiceName = new SearchServiceClient(_appSettings.SearchServiceName, new SearchCredentials(_appSettings.SearchServiceQueryApiKey));
-
-            if (_searchIndexClient == null)
-                _searchIndexClient = _searchServiceName.Indexes.GetClient(_appSettings.SearchIndexName);
-        }
-
-        public async Task<ActionResult> Index([FromQuery] SearchData model)
-        {
-            // Ensure the search string is valid.
-            if (model.SearchText == null)
-            {
-                model.SearchText = "";
-            }
-
-            // Set default result layout
-            if (model.Layout == null)
-            {
-                model.Layout = DisplayMode.Grid;
-            }
-
-            // Make the search call for the first page.
-            await RunQueryAsync(model);
-
-            return View(model);
+            return Redirect(HttpContext.Request.PathBase + "/s");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        private async Task<ActionResult> RunQueryAsync(SearchData model)
-        {
-            InitSearch();
-
-            string filterExpression;
-            try
-            {
-                filterExpression = GetFilterExpression(model.LocationFilter, model.TagFilter, model.SpatialFilter, model.MinDateTaken, model.MaxDateTaken);
-            }
-            catch (ArgumentException ex)
-            {
-                filterExpression = "false";
-            }
-            int page = model.Page ?? 0;
-
-            var parameters = new SearchParameters
-            {
-                Filter = filterExpression,
-
-                // Return information on the text, and number, of facets in the data.
-                Facets = new List<string> { "LocationName,count:20", "Tag,count:20" },
-
-                // Enter media property names into this list, so only these values will be returned.
-                Select = new[] { "Id", "Name", "Project", "LocationName", "Tag", "Location", "ThumbnailURL" },
-
-                // Skip past results that have already been returned.
-                Skip = page * GlobalVariables.ResultsPerPage,
-
-                // Take only the next page worth of results.
-                Top = GlobalVariables.ResultsPerPage,
-
-                // Include the total number of results.
-                IncludeTotalResultCount = true,
-            };
-
-            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
-            model.ResultList = await _searchIndexClient.Documents.SearchAsync<MediaItem>(model.SearchText, parameters);
-
-            // This variable communicates the total number of pages to the view.
-            model.PageCount = ((int)model.ResultList.Count + GlobalVariables.ResultsPerPage - 1) / GlobalVariables.ResultsPerPage;
-
-            // This variable communicates the page number being displayed to the view.
-            model.CurrentPage = page;
-
-            int leftMostPage = 0;
-            // Calculate the range of page numbers to display.
-            if (page == 0)
-            {
-                leftMostPage = 0;
-            }
-            else
-            if (page <= leftMostPage)
-            {
-                // Trigger a switch to a lower page range.
-                leftMostPage = Math.Max(page - GlobalVariables.PageRangeDelta, 0);
-            }
-            else
-            if (page >= leftMostPage + GlobalVariables.MaxPageRange - 1)
-            {
-                // Trigger a switch to a higher page range.
-                leftMostPage = Math.Min(page - GlobalVariables.PageRangeDelta, model.PageCount - GlobalVariables.MaxPageRange);
-            }
-            model.LeftMostPage = leftMostPage;
-
-            // Calculate the number of page numbers to display.
-            model.PageRange = Math.Min(model.PageCount - leftMostPage, GlobalVariables.MaxPageRange);
-
-            if (model.SpatialCategories == null)
-            {
-                model.SpatialCategories = _geoSearchHelper.GetDictionary().Select(s => new SelectListItem(s.Value.Name, s.Value.Id)).ToList(); ;
-            }
-
-            // Return the new view.
-            return View("Index", model);
-        }
-
-        private string GetFilterExpression(IList<string> locationFilter, IList<string> tagFilter, string spatialFilter, long? minDateTaken, long? maxDateTaken)
-        {
-            var subexpressions = new List<string>();
-
-            if (locationFilter != null && locationFilter.Count > 0)
-            {
-                subexpressions.Add(string.Join(" or ", locationFilter.Select(location => $"LocationName eq '{location.Replace("'", "''")}'")));
-            }
-
-            if (tagFilter != null && tagFilter.Count > 0)
-            {
-                subexpressions.Add(string.Join(" or ", tagFilter.Select(tag => $"Tag/any(t: t eq '{tag.Replace("'", "''")}')")));
-            }
-
-            if (minDateTaken != null)
-            {
-                string minDateTakenString = DateTimeOffset.FromUnixTimeSeconds(minDateTaken.Value).ToString("yyyy-MM-dd'T'HH:mm:ss.fffK", CultureInfo.InvariantCulture);
-                subexpressions.Add($"DateTaken ge {minDateTakenString}");
-            }
-
-            if (maxDateTaken != null)
-            {
-                string maxDateTakenString = DateTimeOffset.FromUnixTimeSeconds(maxDateTaken.Value).ToString("yyyy-MM-dd'T'HH:mm:ss.fffK", CultureInfo.InvariantCulture);
-                subexpressions.Add($"DateTaken le {maxDateTakenString}");
-            }
-
-            string spatialExpression = TransformSpatialFilter(spatialFilter);
-            if (!string.IsNullOrEmpty(spatialExpression))
-            {
-                subexpressions.Add(spatialExpression);
-            }
-
-            return string.Join(" and ", subexpressions);
-        }
-
-        private string TransformSpatialFilter(string spatialFilter)
-        {
-            if (string.IsNullOrEmpty(spatialFilter))
-            {
-                return null;
-            }
-
-            _geoSearchHelper.GetDictionary().TryGetValue(spatialFilter, out var ret);
-
-            if (ret == null)
-            {
-                throw new ArgumentException("Could not find coordinates for given area");
-            }
-
-            return "geo.intersects(Location, geography'" + ret.WktPolygon + "')";
         }
     }
 }
