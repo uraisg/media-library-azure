2f598bf702343bdcf047d3c8fe53da2c7e0e33ea
Teo Kah Hou <khteo@users.noreply.github.com>
2022-02-11 20:05:45 +0800

    fix(intranet): Remove more leftover files and code for old home page

--- intranet-webapp/MediaLibrary.Intranet.Web/webpack.mix.js
+++ intranet-webapp/MediaLibrary.Intranet.Web/webpack.mix.js
@@ -18,7 +18,5 @@ mix.sourceMaps(false) // enable source maps in dev mode, but not prod
 mix
   .js('assets/scripts/site.js', 'scripts')
   .js('assets/scripts/item.js', 'scripts')
-  .js('assets/scripts/search.js', 'scripts')
-  .js('assets/scripts/map.js', 'scripts')
   .sass('assets/styles/site.scss', 'styles')
 mix.js('assets/scripts/gallery/main.js', 'scripts/gallery.js').react()
