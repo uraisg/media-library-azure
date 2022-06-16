0a4c187d4d065c30d3510982f51a2bfaef90f8a8
CruzChua <cruzchua2001@gmail.com>
2022-06-16 14:34:37 +0800

    Added Query Files

---
+++ intranet-webapp/MediaLibrary.Intranet.Web/Common/DatabaseQuery(Admin)/QueryAllResult.sql
@@ -0,0 +1,14 @@
+﻿--Retrieve all results
+select * 
+from PlanningArea pa, FileDetails fd
+where pa.AreaPolygon.STContains(fd.AreaPoint) = 0;
+
+--Find the location
+select * 
+from PlanningArea pa, FileDetails fd
+where pa.AreaPolygon.STContains(fd.AreaPoint) = 0 and fd.FileId = 'o1uZrriYsKLDxiY8';
+
+--Filter By Place
+select * 
+from PlanningArea pa, FileDetails fd
+where pa.AreaPolygon.STContains(fd.AreaPoint) = 0 and pa.PlanningAreaName = 'BUKIT PANJANG';
