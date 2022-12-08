a24a770375f6b32be4733ba056da9d2dd2b7e855
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-12-08 17:42:06 +0800

    Remove unused import

--- intranet-webapp/MediaLibrary.Intranet.Web/assets/scripts/gallery/components/ToggleSwitch.js
+++ intranet-webapp/MediaLibrary.Intranet.Web/assets/scripts/gallery/components/ToggleSwitch.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Switch from '@mui/material/Switch';
 import { styled } from '@linaria/react'
