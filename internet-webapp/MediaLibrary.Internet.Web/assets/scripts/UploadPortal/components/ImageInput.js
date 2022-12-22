e550afabf0b5ba3bb3973fbc7597ea571b63f6cb
Teo Kah Hou <khteo@users.noreply.github.com>
2022-12-22 18:14:25 +0800

    feat: Refine Step 1 UX
    - Add help text for maximum file size and supported file types
    - Adjust vertical spacing between elements
    - Use CSS to label required fields

--- internet-webapp/MediaLibrary.Internet.Web/assets/scripts/UploadPortal/components/ImageInput.js
+++ internet-webapp/MediaLibrary.Internet.Web/assets/scripts/UploadPortal/components/ImageInput.js
@@ -1,46 +1,57 @@
 import React from 'react'
+import { FilePond, registerPlugin } from 'react-filepond'
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size'
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
+import 'filepond/dist/filepond.min.css'
 
 import { useForm } from '@/components/AllContext'
 
-// Import React FilePond
-import { FilePond, registerPlugin } from 'react-filepond'
+// Register the FilePond plugins
+registerPlugin(
+  FilePondPluginFileValidateSize,
+  FilePondPluginFileValidateType,
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview
+)
 
-// Import FilePond styles
-import 'filepond/dist/filepond.min.css'
+const MAX_FILES = 25
+const MAX_FILE_SIZE = '40MB'
+const ACCEPTED_FILE_TYPES = [
+  'image/png',
+  'image/jpg',
+  'image/jpeg',
+  'image/gif',
+  'image/bmp',
+  'image/heic',
+]
 
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
-import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
-import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
-
-// Register the plugins
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginFileValidateType, FilePondPluginFileValidateSize)
-
-// Our app
 const FileInput = () => {
   const formContext = useForm()
 
   return (
-    <div className="App">
+    <div className="mb-3">
+      <p>Select up to {MAX_FILES} images to upload</p>
       <FilePond
-        allowFileTypeValidation={true}
-        acceptedFileTypes={['image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image/bmp', 'image/heic']}
+        name="files"
         files={formContext.files}
         onupdatefiles={formContext.setFiles}
         allowMultiple={true}
-        maxFiles={25}
-        name="files"
-        labelIdle='Drag & Drop your files or Browse'
+        maxFiles={MAX_FILES}
+        allowFileTypeValidation={true}
+        acceptedFileTypes={ACCEPTED_FILE_TYPES}
         allowFileSizeValidation={true}
-        maxFileSize='40MB'
+        maxFileSize={MAX_FILE_SIZE}
+        labelIdle='Drag images here or <span class="filepond--label-action">browse</span>'
       />
+      <small className="form-text text-secondary">
+        Each image should be less than 40 MB. Supported file types:{' '}
+        {ACCEPTED_FILE_TYPES.map((_) => _.substring(_.indexOf('/') + 1)).join(', ')}
+      </small>
     </div>
   )
 }
 
 export default FileInput
-
