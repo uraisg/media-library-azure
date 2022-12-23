875c3d8f7612a880a2d015ab6e49127f72619970
Teo Kah Hou <khteo@users.noreply.github.com>
2022-12-23 11:56:19 +0800

    chore: remove code obsoleted by uploadv2

--- internet-webapp/MediaLibrary.Internet.Web/webpack.mix.js
+++ internet-webapp/MediaLibrary.Internet.Web/webpack.mix.js
@@ -3,9 +3,6 @@ const path = require('path')
 
 mix.setPublicPath(path.normalize('wwwroot'))
 mix.webpackConfig({
-  externals: {
-    jquery: 'jQuery',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'assets/scripts/UploadPortal'),
@@ -30,12 +27,9 @@ mix
     [
       'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
       'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/jquery-validation/dist/jquery.validate.min.js',
-      'node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js',
     ],
     'wwwroot/scripts'
   )
-  .js('assets/scripts/upload.js', 'scripts')
   .sass('assets/styles/site.scss', 'styles')
 mix.
   js('assets/scripts/UploadPortal/main.js', 'scripts/upload_portal.js')
