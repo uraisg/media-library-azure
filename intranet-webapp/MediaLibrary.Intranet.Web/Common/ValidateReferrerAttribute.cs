uraisg/media-library-azure

commit 0cf60e2d6e81c1d18ddc116c4908bf69f133c864

fix: skip port comparison when HttpRequest.Host.Port is null (#355)
HttpRequest.Host.Port may sometimes be null. If so, do not compare port number against Origin/Referrer header.

intranet-webapp/MediaLibrary.Intranet.Web/Common/ValidateReferrerAttribute.cs
@@ -43,7 +43,8 @@ public class ValidateOriginAttribute : Attribute, IAuthorizationFilter
         }
 
         // Compare the source against the expected target origin in Host header
-        if (!context.HttpContext.Request.Host.Equals(HostString.FromUriComponent(sourceUri)))
+        if (string.Equals(context.HttpContext.Request.Host.Host, sourceUri.Host, StringComparison.OrdinalIgnoreCase) &&
+            (context.HttpContext.Request.Host.Port != null && context.HttpContext.Request.Host.Port != sourceUri.Port))
         {
             // Origins are not matching so we block the request
             context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);

commit a32d672c4f67d1da2d9f43ed33dc66adbc4c4c45

fix(iz): Fix bad logic in ValidateOriginAttribute
Regression introduced in 0cf60e2

intranet-webapp/MediaLibrary.Intranet.Web/Common/ValidateReferrerAttribute.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -43,7 +43,7 @@ public class ValidateOriginAttribute : Attribute, IAuthorizationFilter
         }
 
         // Compare the source against the expected target origin in Host header
-        if (string.Equals(context.HttpContext.Request.Host.Host, sourceUri.Host, StringComparison.OrdinalIgnoreCase) &&
+        if (!string.Equals(context.HttpContext.Request.Host.Host, sourceUri.Host, StringComparison.OrdinalIgnoreCase) ||
             (context.HttpContext.Request.Host.Port != null && context.HttpContext.Request.Host.Port != sourceUri.Port))
         {
             // Origins are not matching so we block the request